ac3caabcc47066c8ce33c9bdad7b7edcb4115640
Khaly17 <diengkhaly17@gmail.com>
2021-12-19 19:04:10 +0000

    ajout fichier footer file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './component/ListItem/Footer'
 import Index from './component/ListItem/Index'
 import NavBar from './component/ListItem/NavBar'
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <NavBar/>
       <Index/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/component/ListItem/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div class="container">
+            <footer class="py-3 my-4">
+                <p class="text-center text-muted">&copy; 2021 TDSI | Khaly DIENG</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
